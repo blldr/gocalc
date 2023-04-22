701e7c80512dac6449b0d8405cc2c82ec6479b91
Kerem Akçalıoğlu <blldr001@gmail.com>
2023-04-22 22:27:18 +0300

    fucker

--- grapher.go
+++ grapher.go
@@ -2,8 +2,7 @@ package gocalc
 
 import (
 	"fmt"
-	"math"
-	"strconv"
+	// "math"
 	"strings"
 
 	"github.com/blldr/gosolve"
@@ -12,26 +11,41 @@ import (
 type (
 	Graph struct {
 		width      int
-		points     [][][]float64
+		points     []EqPoints
 		resolution int
 	}
+	Point struct {
+		X float64
+		Y float64
+	}
+	EqPoints struct {
+		EqType EqType
+		Color  string
+		Points []Point
+	}
+	EqType int
+)
+
+const (
+	equality EqType = iota
+	greater
+	smaller
 )
 
 func NewGraph(width int, resolution int) *Graph {
 	return &Graph{
 		width,
-		make([][][]float64, 0, 10),
+		make([]EqPoints, 0, 10),
 		resolution,
 	}
 }
 
-func (g *Graph) AddEquation(eqString string, iStart, iEnd float64, prefixData [][]float64) (int, error) {
+func (g *Graph) AddEquation(eqString string, iStart, iEnd float64, EqType EqType, color string) (int, error) {
 	pointsSlice, err := g.createPointsSlice(eqString, iStart, iEnd)
 	if err != nil {
 		return 0, err
 	}
-	pointsSlice = append(prefixData, pointsSlice...)
-	g.points = append(g.points, pointsSlice)
+	g.points = append(g.points, EqPoints{EqType, color, pointsSlice})
 	return len(g.points) - 1, nil
 }
 
@@ -42,7 +56,7 @@ func (g *Graph) RemoveEquation(eqId int) {
 	g.points = append(g.points[:eqId], g.points[eqId+1:]...)
 }
 
-func (g *Graph) ChangeEquation(eqId int, eqString string, iStart, iEnd float64, prefixData [][]float64) error {
+func (g *Graph) ChangeEquation(eqId int, eqString string, iStart, iEnd float64, eqType EqType, color string) error {
 	if eqId >= len(g.points) {
 		return nil
 	}
@@ -50,18 +64,17 @@ func (g *Graph) ChangeEquation(eqId int, eqString string, iStart, iEnd float64, 
 	if err != nil {
 		return err
 	}
-	pointsSlice = append(prefixData, pointsSlice...)
-	g.points[eqId] = pointsSlice
+	g.points[eqId] = EqPoints{eqType, color, pointsSlice}
 	return nil
 }
 
-func (g *Graph) GetPointsSlice() [][][]float64 {
+func (g *Graph) GetPoints() []EqPoints {
 	return g.points
 }
 
-func (g *Graph) createPointsSlice(eqString string, iStart, iEnd float64) ([][]float64, error) {
+func (g *Graph) createPointsSlice(eqString string, iStart, iEnd float64) ([]Point, error) {
 	xCount := g.width * g.resolution
-	pointsSlice := make([][]float64, 0, xCount)
+	pointsSlice := make([]Point, 0, xCount)
 	if iStart > iEnd {
 		iStart, iEnd = iEnd, iStart
 	}
@@ -73,32 +86,40 @@ func (g *Graph) createPointsSlice(eqString string, iStart, iEnd float64) ([][]fl
 	}
 	for i := 0.0; i < float64(xCount); i++ {
 		x := i/float64(g.resolution) - float64(g.width/2)
-		x = math.Round(x*100) / 100
+		// x = math.Round(x*1000) / 1000
 		if x < iStart || x > iEnd {
-			pointsSlice = append(pointsSlice, make([]float64, 0))
 			continue
 		}
 		var eq string
-		xFloat := strconv.FormatFloat(x, 'f', 2, 64)
-		eq = strings.ReplaceAll(eqString, "x", fmt.Sprintf("(%s)", xFloat))
+		eq = strings.ReplaceAll(eqString, "x", fmt.Sprintf("(%f)", x))
 
-		yValues, err := gosolve.FindRoots(eq, -float64(g.width)/2, float64(g.width)/2, 'y')
+		var yValues []float64
+		var err error
+		yValues, err = gosolve.FindRoots(eq, iStart, iEnd, 'y')
+
 		if err != nil {
 			return nil, err
 		}
-		pointsSlice = append(pointsSlice, yValues)
+		for j := 0; j < len(yValues); j++ {
+			pointsSlice = append(pointsSlice, Point{x, yValues[j]})
+		}
 	}
 	for i := 0.0; i < float64(xCount); i++ {
 		y := float64(g.width/2) - i/float64(g.resolution)
-		y = math.Round(y*100) / 100
+		// y = math.Round(y*100) / 100
 		var eq string
-		yFloat := strconv.FormatFloat(y, 'f', 2, 64)
-		eq = strings.ReplaceAll(eqString, "y", fmt.Sprintf("(%s)", yFloat))
-		xValues, err := gosolve.FindRoots(eq, iStart, iEnd, 'x')
+		eq = strings.ReplaceAll(eqString, "y", fmt.Sprintf("(%f)", y))
+		var xValues []float64
+		var err error
+		xValues, err = gosolve.FindRoots(eq, iStart, iEnd, 'x')
+
 		if err != nil {
 			return nil, err
 		}
-		pointsSlice = append(pointsSlice, xValues)
+		for j := 0; j < len(xValues); j++ {
+			pointsSlice = append(pointsSlice, Point{xValues[j], y})
+
+		}
 
 	}
 	return pointsSlice, nil
